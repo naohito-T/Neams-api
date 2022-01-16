1609d6fece78a4a50fb7f0be09ff497793b9a69f
naohito-T <naohito.tanaka0523@gmail.com>
2022-01-16 23:30:11 +0900

    :white_check_mark: users api作成

---
+++ app/controllers/api/v1/home/users_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::Home::UsersController < ApplicationController
+  # user一覧を取得する
+  def index
+    users = User.all
+    # only: [取得するカラム名] ... as_jsonメソッドのオプション機能。
+    render json: users.as_json(only: [:id, :name, :email, :created_at])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :home do
-        namespace :test do
-          resources :hello, only:[:index]
-        end
+        resources :users, only:[:index]
       end
     end
   end
